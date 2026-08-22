98f9905f5a919f744f0f27c7cb264a8baa26a9d6
maxime lapouge <dpower632@gmail.com>
2026-08-22 12:00:07 +0200

    Refactor ActivityDashboard to use mock activities; enhance styling for better layout

--- src/Components/Dashboards/Activity/ActivityDasboard/ActivityDasboard.jsx
+++ src/Components/Dashboards/Activity/ActivityDasboard/ActivityDasboard.jsx
@@ -1,33 +1,52 @@
 import s from "./ActivityDasboard.module.scss";
 import ActivityCard from "../ActivityCard/ActivityCard";
 import EmptyState from "@dashboards/EmptyState/EmptyState";
-export default function ActivityDashboard({ onView, activities = [] }) {
-  activities = [
-    {
-      id: 1,
-      action: "created",
-      entityType: "project",
-      entityId: 12,
-      entityName: "Portfolio",
-      createdAt: "2026-08-21T09:30:00Z",
-    },
-    {
-      id: 2,
-      action: "updated",
-      entityType: "project",
-      entityId: 8,
-      entityName: "VeilleTechno",
-      createdAt: "2026-08-21T08:45:00Z",
-    },
-    {
-      id: 3,
-      action: "created",
-      entityType: "resource",
-      entityId: 23,
-      entityName: "Three.js Journey",
-      createdAt: "2026-08-20T17:20:00Z",
-    },
-  ];
+const mockActivities = [
+  {
+    id: 1,
+    action: "created",
+    entityType: "project",
+    entityId: 12,
+    entityName: "Portfolio",
+    createdAt: "2026-08-21T09:30:00Z",
+  },
+  {
+    id: 2,
+    action: "updated",
+    entityType: "project",
+    entityId: 8,
+    entityName: "VeilleTechno",
+    createdAt: "2026-08-21T08:45:00Z",
+  },
+  {
+    id: 3,
+    action: "created",
+    entityType: "resource",
+    entityId: 23,
+    entityName: "Three.js Journey",
+    createdAt: "2026-08-20T17:20:00Z",
+  },
+  {
+    id: 4,
+    action: "created",
+    entityType: "resource",
+    entityId: 23,
+    entityName: "Three.js Journey",
+    createdAt: "2026-08-20T17:20:00Z",
+  },
+  {
+    id: 5,
+    action: "created",
+    entityType: "resource",
+    entityId: 23,
+    entityName: "Three.js Journey",
+    createdAt: "2026-08-20T17:20:00Z",
+  },
+];
+export default function ActivityDashboard({
+  onView,
+  activities = mockActivities,
+}) {
   return (
     <section className={s.dashboard}>
       <header className={s.header}>
